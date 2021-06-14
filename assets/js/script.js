14dac298833240c951a825a930fb15fc2cde91b2
Craigharrison79 <craigharrison7917@hotmail.com>
2021-06-14 08:00:55 +0000

    forgot to hit save on last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -223,9 +223,10 @@ function returnToStartPage() {
     const restartBtn = document.getElementById('restart')
     restartBtn.addEventListener('click', function (e) {
         score = 0;
-        const finalScore = document.getElementById('final-score');
+        return window.location.href = "/index.html";
+        /* const finalScore = document.getElementById('final-score');
         finalScore.style.display = 'none';
-        document.getElementById('landing-page-container').style.display = 'flex';
+        document.getElementById('landing-page-container').style.display = 'flex';*/
     });
 
 };//END
